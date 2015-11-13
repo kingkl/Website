aebe9428cd0f4fed6a4d36f6e547952f07570a33
Kandice King <kandiceking@bellsouth.net>
2015-11-13 12:56:17 -0500

    new coursework page

--- coursework.php
+++ coursework.php
@@ -21,50 +21,46 @@
 		<div class="content coursework">
 			<h2>Gwinnett Technical College Coursework</h2>
 			<div class="topborder">&nbsp;</div>
-			<div><h3>FALL 2015</h3>
-				<div class="shapeCircleOutside floatright1 positionRelative2"><div class="shapeCircle">&nbsp;</div></div>
+			
+			<div class="homefloatright">
+				<p><img src="images/school.png" alt="Coursework image">
+						<span>Coursework</span></p>
+			</div>
+			
+			<div><h3 class="toppaddingh3">CURRENT CLASSES (FALL 2015)</h3>
+				
 				<ul>
 					<li><span class="title-font"><a href="cist2550/index.html">CIST2550</a></span> - Web Development II</li>
 					<li><span class="title-font">CIST2950</span> - Web Systems Project</li>
 				</ul>
+				<br>
 			</div>
 			
-			<div><h3 class="midborder">SUMMER 2015</h3>
-				<div class="squareBox floatright1 positionRelative3"><span class="shapeSquare">&nbsp;</span></div>
+			
+			<div><h3>PREVIOUS CLASSES WITH WEBSITES</h3>
+				
 				<ul>
-					<li><span class="title-font"><a href="cist2351/index.html">CIST2351</a></span> - PHP Programming I</li>
-				</ul>
-			</div>
-			<div><h3 class="midborder">SPRING 2015</h3>
-				<div class="shapeParallelogram floatright1 positionRelative2">&nbsp;</div>
-				<ul>
-					<li><span class="title-font"><a href="cist1540/index.html">CIST1540</a></span> - Web Animation I (Adobe Flash)</li>
-					<li><span class="title-font">CIST2921</span> - IT Analysis, Design & Project Management</li>
-				</ul>
-			</div>
-			<div><h3 class="midborder">FALL 2014</h3>
-				<div class="borderBox floatright1 positionRelative1"><span class="shapeBorder">&nbsp;</span></div>
-				<ul>
-					<li><span class="title-font">CIST1220</span> - Structured Query Language - SQL</li>
+					<li><span class="title-font"><a href="cist1510/index.html">CIST1510</a></span> - Web Development I (HTML5/CSS3)</li>
 					<li><span class="title-font"><a href="cist1520/index.html">CIST1520</a></span> - Scripting Technologies (JavaScript)</li>
+					<li><span class="title-font"><a href="cist1530/index.html">CIST1530</a></span> - Web Graphics I (Adobe Photoshop)</li>
+					<li><span class="title-font"><a href="cist1540/index.html">CIST1540</a></span> - Web Animation I (Adobe Flash)</li>
+					<li><span class="title-font"><a href="cist2351/index.html">CIST2351</a></span> - PHP Programming I</li>
 					<li><span class="title-font"><a href="cist2510/index.html">CIST2510</a></span> - Web Technologies (Adobe Dreamweaver)</li>
 					<li><span class="title-font"><a href="cist2531/index.html">CIST2531</a></span> - Web Graphics II (Advanced Adobe Photoshop)</li>
+					
 				</ul>
+				<br>
 			</div>
-			<div><h3 class="midborder">SUMMER 2014</h3>
-				<div class="shapeMisc floatright1 positionRelative2">&nbsp;</div>
+			<div><h3>PREVIOUS CLASSES WITHOUT WEBSITES</h3>
+				
 				<ul>
-					<li><span class="title-font"><a href="cist1530/index.html">CIST1530</a></span> - Web Graphics I (Adobe Photoshop)</li>
-					<li><span class="title-font">CIST1601</span> - Information Security Fundamentals</li>
-				</ul>
-			</div>
-			<div><h3 class="midborder">SPRING 2014</h3>
-				<div class="shapeBigSquareOutSide floatright1"><div class="shapeSquareRotated">&nbsp;</div></div>
-				<ul>
-					<li><span class="title-font">CIST1001</span> - Computer Concepts</li>
-					<li><span class="title-font">CIST1305</span> - Program Design and Development</li>
-					<li><span class="title-font"><a href="cist1510/index.html">CIST1510</a></span> - Web Development I (HTML5/CSS3)</li>
 					<li><span class="title-font">COMP1000</span> - Introduction to Computers</li>
+					<li><span class="title-font">CIST1001</span> - Computer Concepts</li>
+					<li><span class="title-font">CIST1220</span> - Structured Query Language - SQL</li>
+					<li><span class="title-font">CIST1305</span> - Program Design and Development</li>
+					<li><span class="title-font">CIST1601</span> - Information Security Fundamentals</li>
+					<li><span class="title-font">CIST2921</span> - IT Analysis, Design & Project Management</li>
+					
 				</ul>
 			
 			</div>
